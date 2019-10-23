ca3f133b516f0318f314257fee408e2ce9dc7ebd
Dean Jeffery <itsdeanrj@gmail.com>
2019-10-23 09:39:35 +0100

    first commit
    - skeleton of first pages
    - start of login form

---
+++ inc/connection.inc.php
@@ -0,0 +1,12 @@
+<?php
+// {
+//   $db = new mysqli('localhost','root','root','djeffery_web-security-practical');
+//   if (mysqli_connect_errno())
+//   {
+//     echo "Error: Could not connect to the database. Please try again later";
+//     exit;
+//   }
+// }
+
+$connection = mysqli_connect('localhost','root','root');
+?>
